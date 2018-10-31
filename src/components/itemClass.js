109fe5d459c484b0c43f022e6ae5499e9f7bd25b
Justin Small <justinsmall@mac.com>
2018-10-31 11:12:48 +0000

    updates

---
+++ src/components/itemClass.js
@@ -0,0 +1,70 @@
+export const item = {
+  classes: {
+    applied: ['o-item'],
+    imageSwap: 'o-item__imageSwap',
+    headerSwap: 'o-item__headerSwap',
+    imageRotation: 'o-item__imageRotation',
+    card: 'o-item--card',
+    fullWidth: 'o-item--100vw',
+    tintHover: 'o-item--tintHover'
+  },
+  image: {
+    link: false,
+    linkUrl: '',
+    url: 'https://www.jackwills.com/on/demandware.static/-/Sites/default/dw0c9b5508/2018/WK38/01_MEGADROP/MD_Gifting.jpg',
+    classes: {
+      fullWidth: 'o-item__image--100vw'
+    }          
+  },
+  content: {
+    heading: [
+      {
+        el: 'h2',
+        text: 'This is a heading',
+        link: false,
+        linkUrl: '',
+        classes: {
+          applied: ['o-hdr', 'o-hdr--sm', 'o-item__content', 'o-item__hdr']
+        },
+        edit: false
+      },
+      {
+        el: 'h3',
+        text: 'This is a heading',
+        link: false,
+        linkUrl: '',
+        classes: {
+          applied: ['o-hdr', 'o-hdr--lg', 'o-item__content', 'o-item__hdr']
+        },
+        edit: false
+      }          
+    ],
+    subCopy: `This is some sub copy`,
+    buttons: {
+      button: [
+        {
+          linkUrl: '',
+          text: 'Shop Now',
+          classes: {
+            applied: ['o-btn', 'o-btn--basic']
+          }
+        },
+        {
+          linkUrl: '',
+          text: 'Shop Now',
+          classes: {
+            applied: ['o-btn', 'o-btn--basic']
+          }
+        }            
+      ],
+      classes: {
+        applied: ['o-buttons']
+      }
+    },
+    classes: {
+
+    }          
+  }                 
+}
+
+
