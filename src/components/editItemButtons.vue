b7a64601b3fb6c6fe937faa688aec4a0d869012c
Justin Small <justinsmall@mac.com>
2018-12-20 21:01:01 +0000

    more refactor

--- src/components/editItemButtons.vue
+++ src/components/editItemButtons.vue
@@ -3,60 +3,61 @@
 
     <a 
       class="c-btn-tag o-btn o-btn--plain o-btn--sm active" 
-      v-for="(CssClass, index) in items[editPanel.itemIndex].content.buttons.classes.applied"             
-      @click="applyClass(index, items[editPanel.itemIndex].content.buttons.classes, 'remove')">
+      v-for="(CssClass, index) in buttonsClassesApplied"             
+      @click="applyClass(index, buttonsClasses, 'remove')">
       {{ CssClass }}
     </a>      
     
     <hr>
 
     <a class="c-btn-tag o-btn o-btn--plain o-btn--sm" 
-      v-for="(item, index) in items[editPanel.itemIndex].content.buttons.classes.available"
-      v-if="item.class[2] == true && !item.class[0].includes('wide') && !item.class[0].includes('column')"   
-      @click="applyClass(index, items[editPanel.itemIndex].content.buttons.classes, 'apply')">
+      v-for="(item, index) in cssClassLinksToApply"    
+      @click="applyClass(item.index, buttonsClasses, 'apply')">
       {{ item.class | abbrClass }}
-    </a> 
+    </a>     
+
+    <!-- <custom-button button plain small 
+      class="c-btn-tag"
+      v-for="(item, index) in cssClassLinksToApply"  
+      @click="applyClass(item.index, buttonsClasses, 'apply')"  
+      >
+      {{ item.class | abbrClass }}
+    </custom-button> -->
+
 
     <label>Width (optional)</label>
     <select v-model="buttonsWidthSelected">
       <option selected>{{ buttonsWidthSelected }}</option>
-      <option 
-        v-for="(item, index) in items[editPanel.itemIndex].content.buttons.classes.available" 
-        v-if="item.class[2] == true && item.class[0].includes('wide')">
-        {{ item.class[0] }}
-      </option>
-    </select>   
+      <option v-for="(item, index) in buttonWidthClasses">{{ item.class[0] }}</option>
+    </select>       
+
 
     <label>Columns (optional)</label>
     <select v-model="buttonsColumnsSelected">
       <option selected>{{ buttonsColumnsSelected }}</option>
-      <option 
-        v-for="(item, index) in items[editPanel.itemIndex].content.buttons.classes.available" 
-        v-if="item.class[2] == true && item.class[0].includes('column')">
-        {{ item.class[0] }}
-      </option>
+      <option v-for="(item, index) in buttonColumnClasses">{{ item.class[0] }}</option>
     </select>                      
 
-    <div v-for="(button, index) in items[editPanel.itemIndex].content.buttons.button">
+    <div v-for="(button, index) in buttonsButton">
       <h3 class="o-hdr o-hdr--t u-mt-t">Button {{index + 1}}</h3>                     
       <select v-model="buttonEdit" class="minimal">
         <option selected>Select property to edit</option>
         <option 
-          v-for="(value, key, index) in items[editPanel.itemIndex].content.buttons.button[editPanel.itemIndex]" 
+          v-for="(value, key, index) in buttonsButton[editPanel.itemIndex]" 
           :key="index">
-          {{ key }}
+          {{ key }}          
         </option>
       </select>  
       <div v-if="buttonEdit == 'text'">             
         <input 
           type="text" 
-          v-model="items[editPanel.itemIndex].content.buttons.button[index].text" 
+          v-model="buttonsButton[index].text" 
           placeholder="Binded value goes here" />            
       </div>
       <div v-if="buttonEdit == 'linkUrl'">             
         <input 
           type="text" 
-          v-model="items[editPanel.itemIndex].content.buttons.button[index].url" 
+          v-model="buttonsButton[index].url" 
           placeholder="Binded value goes here" />                        
       </div>
     </div>
@@ -66,10 +67,14 @@
 <script>
 
 import { editPanelMixins } from '../mixins/editPanel-applyClass'
+import CustomButton from './button.vue'
 
 export default {
   // Name of this component
   name: 'editItemButtons',
+  components: {
+    CustomButton
+  },
   props: {
     items: Array,
     editPanel: Object
@@ -78,8 +83,30 @@ export default {
     return {
       buttonsWidthSelected: 'Select a width',
       buttonsColumnsSelected: 'Select no of columns',
-      buttonEdit:'Select property to edit'      
+      buttonEdit:'Select property to edit',
+      buttonsClasses: this.items[this.editPanel.itemIndex].content.buttons.classes,
+      buttonsClassesAvailable: this.items[this.editPanel.itemIndex].content.buttons.classes.available,
+      buttonsClassesApplied: this.items[this.editPanel.itemIndex].content.buttons.classes.applied,
+      buttonsButton: this.items[this.editPanel.itemIndex].content.buttons.button,
+      classLinksToApply: []     
     }
+  },
+  computed: {
+    cssClassLinksToApply() {
+      this.buttonsClassesAvailable.map((className, index) => {
+        if((className => className.class[2] == true) && (!className.class[0].includes('wide')) && (!className.class[0].includes('column'))) {
+          className.index = index
+          this.classLinksToApply.push(className)
+        }
+      })      
+      return this.classLinksToApply
+    },
+    buttonWidthClasses() {
+      return this.buttonsClassesAvailable.filter(className => className.class[2] == true && className.class[0].includes('wide'))
+    },
+    buttonColumnClasses() {
+      return this.buttonsClassesAvailable.filter(className => className.class[2] == true && className.class[0].includes('column'))
+    }    
   },
   mixins: [editPanelMixins],    
   watch: { 
@@ -88,6 +115,18 @@ export default {
     },    
     buttonsColumnsSelected: function(newVal, oldVal) { // watch it
       this.applyWidth(this.items[this.editPanel.itemIndex].content.buttons.classes.applied, newVal)
+      // Add buttons if explicit column type selected
+      const buttonArr = this.items[this.editPanel.itemIndex].content.buttons.button,
+            buttonRef = JSON.parse(JSON.stringify(buttonArr[0]))
+      if(newVal.includes('three') && buttonArr.length < 3) {
+        buttonArr.push(buttonRef)
+      }
+      if(newVal.includes('four') && buttonArr.length < 4) {
+        while(buttonArr.length < 4) {
+          buttonArr.push(buttonRef)
+        }
+      }       
+      
     }    
   },  
   methods: {
