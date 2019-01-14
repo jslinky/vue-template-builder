1794a5dbcc1038769031cfada9e0814fcdf89dbd
Justin Small <justinsmall@mac.com>
2019-01-14 19:41:13 +0000

    added LESS compile config

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import './test.less'
 
 Vue.config.productionTip = false
 // install axios to global Vue instance - access as $http
